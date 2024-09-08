60c8607dfd9945d7c766dfa35a99c3ce8290af62
승엽 <tmdduqflfl@naver.com>
2024-09-08 16:31:51 +0900

    feat: 스프링 초기셋팅

---
+++ investement/src/main/java/com/example/investement/InvestementApplication.java
@@ -0,0 +1,13 @@
+package com.example.investement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestementApplication.class, args);
+    }
+
+}
---
+++ investement/src/test/java/com/example/investement/InvestementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
