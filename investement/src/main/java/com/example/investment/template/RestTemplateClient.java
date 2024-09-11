05a7d4565b53c7d95f9fa098d14fdfd5534c395d
승엽 <tmdduqflfl@naver.com>
2024-09-11 17:51:12 +0900

    Refactor: RestTemlate 공유를 위한 새로운 패키지

--- investement/src/main/java/com/example/investment/template/RestTemplateClient.java
+++ investement/src/main/java/com/example/investment/template/RestTemplateClient.java
@@ -1,4 +1,4 @@
-package com.example.investment.news.service.client;
+package com.example.investment.template;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpEntity;
