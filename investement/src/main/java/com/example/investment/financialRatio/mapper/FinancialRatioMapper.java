ca9b0ca313cddaa3e872ad96ab964803029b3840
승엽 <tmdduqflfl@naver.com>
2024-09-12 15:44:50 +0900

    Refatctor: 전체적인 구조 변경

--- investement/src/main/java/com/example/investment/financialRatio/mapper/FinancialRatioMapper.java
+++ investement/src/main/java/com/example/investment/financialRatio/mapper/FinancialRatioMapper.java
@@ -11,28 +11,27 @@ import java.util.List;
 @Component
 public class FinancialRatioMapper {
 
-    public void toFinancialRatioDTO(final JSONArray outputArray) {
+    public List<FinancialRatioDTO> toFinancialRatioDTO(final JSONArray outputArray) {
         List<FinancialRatioDTO> financialRatios = new ArrayList<>();
 
-        for (int i = 1; i <= 4; i++) {
-            if (i < outputArray.length()) {
-                JSONObject record = outputArray.getJSONObject(i);
+        for (int i = 0; i < outputArray.length(); i++) {
+            JSONObject record = outputArray.getJSONObject(i);
 
-                FinancialRatioDTO dto = new FinancialRatioDTO(
-                        record.optString("stac_yymm", ""),
-                        record.optString("grs", ""),
-                        record.optString("bsop_prfi_inrt", ""),
-                        record.optString("ntin_inrt", ""),
-                        record.optString("roe_val", ""),
-                        record.optString("eps", ""),
-                        record.optString("sps", ""),
-                        record.optString("bps", ""),
-                        record.optString("rsrv_rate", ""),
-                        record.optString("lblt_rate", "")
-                );
-                financialRatios.add(dto);
-            }
+            FinancialRatioDTO dto = new FinancialRatioDTO(
+                    record.optString("stac_yymm", ""),
+                    record.optString("grs", ""),
+                    record.optString("bsop_prfi_inrt", ""),
+                    record.optString("ntin_inrt", ""),
+                    record.optString("roe_val", ""),
+                    record.optString("eps", ""),
+                    record.optString("sps", ""),
+                    record.optString("bps", ""),
+                    record.optString("rsrv_rate", ""),
+                    record.optString("lblt_rate", "")
+            );
+            financialRatios.add(dto);
         }
 
+        return financialRatios;
     }
 }
