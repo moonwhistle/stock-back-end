e9aacfa3098bb7aefaf76943a8febce781262529
Hheojiwon <0618amy@naver.com>
2024-09-12 17:50:26 +0900

    feat: 등락률 response

---
+++ investement/src/main/java/com/example/investment/home/fluctuation/controller/dto/response/fluctuationResponse.java
@@ -0,0 +1,16 @@
+package com.example.investment.home.fluctuation.controller.dto.response;
+
+
+import java.util.List;
+
+public class fluctuationResponse {
+    private List<fluctuationDTO> output;
+
+    public List<fluctuationDTO> getOutput() {
+        return output;
+    }
+
+    public void setOutput(List<fluctuationDTO> output) {
+        this.output = output;
+    }
+}
