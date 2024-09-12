fb987fc2479e32d487b8c648b3a36e236ac0aaf9
승엽 <tmdduqflfl@naver.com>
2024-09-13 03:00:20 +0900

    Refactor: 객체지향적으로 패키지 구조 변경

--- investement/src/main/java/com/example/investment/search/news/service/client/NewsFetcher.java
+++ investement/src/main/java/com/example/investment/search/news/service/client/NewsFetcher.java
@@ -1,16 +1,18 @@
 package com.example.investment.search.news.service.client;
 
+import com.example.investment.common.RestTemplateClient;
 import org.json.JSONException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewsFetcher {
 
-    private final NewsApiService newsApiService;
+    private final RestTemplateClient restTemplateClient;
 
     @Value("${naver.api.clientId}")
     private String clientId;
@@ -18,14 +20,14 @@ public class NewsFetcher {
     @Value("${naver.api.clientSecret}")
     private String clientSecret;
 
-    public NewsFetcher(NewsApiService newsApiService) {
-        this.newsApiService = newsApiService;
+    public NewsFetcher(final RestTemplateClient restTemplateClient) {
+        this.restTemplateClient = restTemplateClient;
     }
 
     public ResponseEntity<String> fetch(String keyword) throws JSONException {
         String url = "https://openapi.naver.com/v1/search/news.json?query=" + keyword;
         HttpHeaders headers = setHeader();
-        return newsApiService.get(url, headers);
+        return get(url, headers);
     }
 
     private HttpHeaders setHeader() {
@@ -34,4 +36,15 @@ public class NewsFetcher {
         headers.set("X-Naver-Client-Secret", clientSecret);
         return headers;
     }
+
+    public ResponseEntity<String> get(String url, HttpHeaders headers) {
+        setHeader(headers);
+        return restTemplateClient.exchange(url, HttpMethod.GET, headers, String.class);
+    }
+
+    private void setHeader(final HttpHeaders headers) {
+        headers.set("X-Naver-Client-Id", clientId);
+        headers.set("X-Naver-Client-Secret", clientSecret);
+    }
+
 }
