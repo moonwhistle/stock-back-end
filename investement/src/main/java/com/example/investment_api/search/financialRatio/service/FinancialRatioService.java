2401b9497ab8b53fc1ab0a4f6c76b6ba79fd8cf6
승엽 <tmdduqflfl@naver.com>
2024-09-21 02:57:46 +0900

    Refactor: 재무제표 정렬 및 캡슐화 주석 추가

--- investement/src/main/java/com/example/investment_api/search/financialRatio/service/FinancialRatioService.java
+++ investement/src/main/java/com/example/investment_api/search/financialRatio/service/FinancialRatioService.java
@@ -1,16 +1,16 @@
 package com.example.investment_api.search.financialRatio.service;
 
+import com.example.investment_api.common.stockData.Stock;
+import com.example.investment_api.common.stockData.StockRepository;
 import com.example.investment_api.search.financialRatio.controller.dto.FinancialRatioDTO;
-
+import com.example.investment_api.search.financialRatio.service.client.FinancialRatioDataFetcher;
 import com.example.investment_api.search.financialRatio.infrastructure.FinancialRatioParser;
 
-import com.example.investment_api.search.financialRatio.service.client.FinancialRatioDataFetcher;
-
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import jakarta.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
 
@@ -20,19 +20,21 @@ public class FinancialRatioService {
 
     private final FinancialRatioDataFetcher financialRatioDataFetcher;
     private final FinancialRatioParser financialRatioParser;
+    private final StockRepository stockRepository;
 
     @Autowired
-    public FinancialRatioService(FinancialRatioDataFetcher financialRatioDataFetcher, FinancialRatioParser financialRatioParser) {
+    public FinancialRatioService(FinancialRatioDataFetcher financialRatioDataFetcher,
+                                 FinancialRatioParser financialRatioParser,
+                                 StockRepository stockRepository) {
         this.financialRatioDataFetcher = financialRatioDataFetcher;
         this.financialRatioParser = financialRatioParser;
+        this.stockRepository = stockRepository;
     }
 
-    public List<FinancialRatioDTO> getFinancialRatio(String stockInfo) throws IOException {
-        return getFinancialRatioDTOS(stockInfo);
-    }
-
-    private List<FinancialRatioDTO> getFinancialRatioDTOS(final String stockInfo) throws IOException {
-        ResponseEntity<String> response = financialRatioDataFetcher.fetchFinancialRatioData(stockInfo);
+    public List<FinancialRatioDTO> getFinancialRatio(String stockName) throws IOException {
+        Stock stock = stockRepository.findByStockName(stockName)
+                .orElseThrow(() -> new RuntimeException("주식명: " + stockName + "을(를) 찾을 수 없습니다."));
+        ResponseEntity<String> response = financialRatioDataFetcher.fetchFinancialRatioData(stock.getStockCode());
         return financialRatioParser.parseFinancialRatio(response.getBody());
     }
 }
--- investement/src/main/java/com/example/investment_api/search/financialRatio/service/client/FinancialRatioDataFetcher.java
+++ investement/src/main/java/com/example/investment_api/search/financialRatio/service/client/FinancialRatioDataFetcher.java
@@ -14,8 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class FinancialRatioDataFetcher {
 
-    private final RestTemplate restTemplate;
-
     @Value("${financialRatio.tr_id}")
     private String trId;
 
@@ -27,6 +25,8 @@ public class FinancialRatioDataFetcher {
 
     @Value("${api.access_token}")
     private String accessToken;
+
+    private final RestTemplate restTemplate;
 
     public FinancialRatioDataFetcher(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
