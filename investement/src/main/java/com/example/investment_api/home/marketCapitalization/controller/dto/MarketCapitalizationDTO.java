af44224cb2648a755b14dcb3bfa614378d893fb5
승엽 <tmdduqflfl@naver.com>
2024-09-21 02:49:48 +0900

    feat: 시가총액 단위 추가

--- investement/src/main/java/com/example/investment_api/home/marketCapitalization/controller/dto/MarketCapitalizationDTO.java
+++ investement/src/main/java/com/example/investment_api/home/marketCapitalization/controller/dto/MarketCapitalizationDTO.java
@@ -4,6 +4,6 @@ public record MarketCapitalizationDTO(
         String rank, //순위
         String stockPrice, //주식가격
         String stockName, //주식이름
-        String marketCapitalization
+        String marketCapitalization //시가총액 단위 = 000조 000
 ) {
 }
