a64cdda6938be66b7f290a7c59b319b6b631494d
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-09-22 19:28:01 +0900

    Feat: global 예외처리 뼈대 구현.

---
+++ investement/src/main/java/com/example/investment_api/global/exception/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.investment_api.global.exception.exceptionhandler;
+
+import com.example.investment_api.global.exception.exceptionhandler.dto.ErrorResponse;
+import com.example.investment_api.global.exception.exceptions.CustomErrorCode;
+import com.example.investment_api.global.exception.exceptions.CustomException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ClassCastException.class)
+    public ResponseEntity<ErrorResponse> handleException(CustomException e) {
+        CustomErrorCode customErrorCode = e.getCustomErrorCode();
+        ErrorResponse errorResponse = new ErrorResponse(customErrorCode.getCustomCode(), customErrorCode.getMessage());
+        return ResponseEntity.status(customErrorCode.getHttpStatus()).body(errorResponse);
+    }
+}
